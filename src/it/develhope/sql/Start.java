7c52bb97816f27bbcbb4f604eac227a8a0f87141
Alessio Limina <alessio.limina@gmail.com>
2023-03-22 18:52:38 +0100

    Exercise SQL 04 View Updated

--- src/it/develhope/sql/Start.java
+++ src/it/develhope/sql/Start.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class Start {
     public static void main(String[] args) throws SQLException {
-        Connection conn = null;
-        List<Student> italianStudents = new ArrayList<>();
-        List <Student> germanStudents = new ArrayList<>();
 
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "developer", "developerpass1");
+            List<Student> italianStudents = new ArrayList<>();
+            List<Student> germanStudents = new ArrayList<>();
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "developer", "developerpass1");
             Statement statement = conn.createStatement();
 
             //creating and executing the views:
@@ -25,40 +25,36 @@ public class Start {
             String selectDeuStudents = "SELECT * FROM newdb.german_students";
 
             ResultSet rsITA = statement.executeQuery(selectItaStudents);
-            while(rsITA.next()){
+            while (rsITA.next()) {
                 //getting each set result from the queries to create a Student and set values to his variables
                 //then adding it to the arrayList
                 String nome = rsITA.getString("first_name");
                 String cognome = rsITA.getString("last_name");
-                Student student = new Student(nome,cognome);
-                italianStudents.add(student);}
+                Student student = new Student(nome, cognome);
+                italianStudents.add(student);
+            }
 
             ResultSet rsDEU = statement.executeQuery(selectDeuStudents);
-            while(rsDEU.next()){
+            while (rsDEU.next()) {
                 String nome = rsDEU.getString("first_name");
                 String cognome = rsDEU.getString("last_name");
-                Student student = new Student(nome,cognome);
-                germanStudents.add(student);}
+                Student student = new Student(nome, cognome);
+                germanStudents.add(student);
+            }
 
             //printing each student value using getters
             System.out.println("Printing italian students:");
-            for(Student itaStudent : italianStudents){
-                System.out.println("Nome: " + itaStudent.getName() + ", cognome: " + itaStudent.getSurname());}
+            for (Student itaStudent : italianStudents) {
+                System.out.println("Nome: " + itaStudent.getName() + ", cognome: " + itaStudent.getSurname());
+            }
 
             System.out.println("Printing german students:");
-            for(Student deuStudent : germanStudents){
-                System.out.println("Nome: " + deuStudent.getName() + ", cognome: " + deuStudent.getSurname());}
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e2) {
-                System.out.println(e2.getMessage());
+            for (Student deuStudent : germanStudents) {
+                System.out.println("Nome: " + deuStudent.getName() + ", cognome: " + deuStudent.getSurname());
             }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
