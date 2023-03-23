44c398f56186358104569872747df734aa9f0031
Alessio Limina <alessio.limina@gmail.com>
2023-03-23 15:39:58 +0100

    Exercise SQL 04 View Updated

--- src/it/develhope/sql/Start.java
+++ src/it/develhope/sql/Start.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Start {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         try {
             List<Student> italianStudents = new ArrayList<>();
